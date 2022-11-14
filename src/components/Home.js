56c8ce580174528b3c9f282f348a9a830c0dffa7
shital8596@gmail.com <shital8596@gmail.com>
2022-11-14 16:50:33 +0530

    first-commit

---
+++ src/components/Home.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='OuterContainer'>
+        <div className='mainContainer'>
+            <div className='innerDiv'>
+                <div className='logoImg'>
+                    logo
+                </div>
+                <div className='sideLogoDiv'>
+                    sidelogo
+                </div>
+            </div>
+            <div className='textDiv'>
+                Ultimate Protection
+            </div>
+            <div className='secondTextDiv'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec mauris ligula. Vesti fringilla tempor feugiat. Donec hendrerit diam
+            </div>
+            <div className='imageDiv'>
+            <div className='img'>
+            <img src="https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60" alt="no img"/>
+            </div>
+            </div>
+        </div>
+        <div className='main2Container'>
+            <div className='smallDiv'>
+                xxx
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
